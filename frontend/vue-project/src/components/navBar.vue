c2e516eaabf8c9cbe068a17850cc429653e5a7bc
Thalita Reis <thalitafelicianoreis@gmail.com>
2026-03-19 20:34:59 -0300

    Ajustando Frontend

--- frontend/vue-project/src/components/navBar.vue
+++ frontend/vue-project/src/components/navBar.vue
@@ -5,6 +5,10 @@
 
     <div class="links">
 
+      <router-link v-if="tipo === 'funcionario'" to="/funcionario"
+      >Funcionário</router-link>
+      <router-link v-if="tipo === 'paciente'" to="/paciente"
+      >Paciente</router-link>
       <button @click="logout">Sair</button>
 
     </div>
